cc465f019c0f43d117f9c97114ed27b2480613db
Nicola Luongo <j2lcoding@gmail.com>
2022-05-11 20:39:35 -0400

    save

--- app/controllers/raw_events_controller.rb
+++ app/controllers/raw_events_controller.rb
@@ -1,4 +1,6 @@
 class RawEventsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     raw_event = RawEvent.create!(data: params.permit!.to_h)
     render json: raw_event, status: :created
