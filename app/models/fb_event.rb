ffc724d1fee368699e1fe32b1441cde8ab476647
j2l <j2lcoding@gmail.com>
2022-08-29 15:31:47 -0400

    save

--- app/models/fb_event.rb
+++ app/models/fb_event.rb
@@ -1,3 +1,11 @@
 class FbEvent < ApplicationRecord
   validates :data, presence: true
+
+  def message_delivered?
+    data['entry'].first['changes'].first['value']['statuses'].first['status'] == 'delivered' rescue false
+  end
+
+  def message_sent?
+    data['entry'].first['changes'].first['value']['statuses'].first['status'] == 'sent' rescue false
+  end
 end
