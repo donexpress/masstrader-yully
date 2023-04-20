20038e36644ff10224db9372de01188211fa40ab
jose <jose.ruben.machado.vega@gmail.com>
2023-04-20 11:59:27 -0400

    fixing error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile += %w[*.png *.jpg *.jpeg *.gif] 
- 
+  config.assets.compile = false
+
+# https://stackoverflow.com/questions/8052865/rails-3-1-asset-pipeline-why-my-images-do-not-precompile-for-production
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
