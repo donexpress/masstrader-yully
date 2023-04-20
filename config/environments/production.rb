21b6766ed8252789c255c395e18e23ed4c055c61
jose <jose.ruben.machado.vega@gmail.com>
2023-04-20 12:23:35 -0400

    change tags

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-# https://stackoverflow.com/questions/8052865/rails-3-1-asset-pipeline-why-my-images-do-not-precompile-for-production
+  # https://stackoverflow.com/questions/8052865/rails-3-1-asset-pipeline-why-my-images-do-not-precompile-for-production
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
